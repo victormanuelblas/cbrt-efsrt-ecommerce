b0f9454802b75c9e7de9b669551d1ca67afdd992
victormanuelblas <vmblas@gmail.com>
2024-10-17 21:53:22 -0500

    finish-product

---
+++ js/producto.js
@@ -0,0 +1,24 @@
+function addToCart () {
+    let sku = document.getElementById("sku").innerText;
+    let cartElms = localStorage.getItem("cartElms");
+    let elms = (cartElms == null) ? [] : JSON.parse(cartElms);
+    
+    let elmsIndx = elms.findIndex(objc => objc.code == sku)
+
+    if(elmsIndx >= 0){
+        elms[0].qnty += 1;
+    }else{
+        elms.push({
+            code: sku,
+            qnty: 1
+        })    
+    }
+    
+    localStorage.setItem("cartElms",JSON.stringify(elms))
+    alert('Producto agregado');
+}
+
+function deleteItems (){
+    localStorage.removeItem("cartElms");
+    alert("Elementos Eliminados");
+}
